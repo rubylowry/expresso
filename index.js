e8e4f1bceb3be34c1a8505749186ce952fe7b7ac
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-01 22:02:55 +0000

    adding npm installations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();//top-level function of express
 
 const path = require('path');
-const apiData = require('./')
+const apiData = require('./people.json');
 
 const port = 3000;
 
@@ -15,12 +15,21 @@ app.use((req,res,next)=>{
 // app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use(express.static('public'));//all files from public folder must be included
-
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 // set the route for index.html
 app.get('/', (req,res,)=>{
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+// set the route for about.html
+app.get('/about', (req,res,)=>{
+  res.sendFile(path.join(__dirname+'/public/about.html'));
+});
+//give access to apiData
+app.get('/people', (req,res,)=>{
+  res.json(apiData);
+});
 
 
 app.listen(port, () => console.log(`This app is listening on port ${port}!`))
